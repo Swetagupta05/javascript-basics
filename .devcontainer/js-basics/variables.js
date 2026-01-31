5cd8d7133646ccb3b875e1003be1d15506c8516f
Sweta Gupta <khushbugupta2355@gmail.com>
2026-01-31 09:00:20 +0000

    learnt variables in js

---
+++ .devcontainer/js-basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail= "khushbugupta2355@gmail.com"
+var accountPassword = 12345
+accountCity = "Pune" // not prefered to use but it works 
+let accountState; //Its ur wish to use ; or  not
+
+// accountId =2  [Not Allowed]
+accountEmail = "gmail.com"
+accountPassword = 353535353;
+accountCity = "blr"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*summary: always use let or const and var is not mostly used or used in old codes.
+because of issue in functional and block scope
+*/
