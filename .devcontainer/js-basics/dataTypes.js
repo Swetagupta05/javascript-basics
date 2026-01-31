d36d679c3a63388d959872c948ec043b60acd84b
Sweta Gupta <khushbugupta2355@gmail.com>
2026-01-31 09:30:12 +0000

    learnt datatypes

---
+++ .devcontainer/js-basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as a newer version
+// alert(3+3) -> we r using nodejs not browser so there will be no alert pop up
+
+// console.log(3+3); console.log("hitesh") -> code readability shoul be high (do not write code like this)
+let name = "Sweta" //string
+let age = 20; //number
+let isLoggedin=false; //boolean
+
+//number => range: 2^53
+//string => must be writeen in double strings("")
+//bigInt => not mostly used but used in trading 
+// boolean => true or false
+// null  => standolan value (let state=null) => it is object
+//undefined => value is not assigned yet (let state;) =>it is undefined always
+//symbol => uniqueness (use mostly iin react.js and used in figma tools)
+
+console.log(typeof age) //used to now the datatype 
+console.log(typeof null) //object
+console.log(typeof undefined); //undefined 
+
+
+
